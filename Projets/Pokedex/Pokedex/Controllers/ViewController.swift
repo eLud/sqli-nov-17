1b32284610aa8db80f13ba101aab87ef3b9101f8
Ludovic Ollagnier <ludovic@ollagnier.me>
2017-11-30 14:49:50 +0100

    Configuration UI + enregistrement

--- Projets/Pokedex/Pokedex/Controllers/ViewController.swift
+++ Projets/Pokedex/Pokedex/Controllers/ViewController.swift
@@ -16,13 +16,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var typeSegmentedControl: UISegmentedControl!
     @IBOutlet weak var levelSlider: UISlider!
     @IBOutlet weak var capturedSwitch: UISwitch!
+    @IBOutlet weak var photoButton: UIButton!
+    @IBOutlet weak var levelLabel: UILabel!
+
+    //MARK: Instance properties
+    var manager = PokeManager()
 
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
 
-        
+        configureUI()
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,10 +36,50 @@ class ViewController: UIViewController {
 
     //MARK: - Actions
 
+    @IBAction func levelSliderValueChanged(_ sender: UISlider) {
+        let rounded = round(sender.value)
+        sender.value = rounded
+        levelLabel.text = "\(Int(sender.value))"
+    }
+
     @IBAction func save(_ sender: Any) {
+
+        guard let name = nameTextField.text, name.count > 2 else {
+            nameTextField.backgroundColor = UIColor.red
+            return
+        }
+        let type = selectedType()
+        let level = Int(round(levelSlider.value))
+        let captured = capturedSwitch.isOn
+
+        let newPokemon = Pokemon(name: name, level: level, type: type, captured: captured)
+
+        manager.add(newPokemon)
     }
     
     @IBAction func takePhoto(_ sender: Any) {
     }
+
+    //MARK: - Private methods
+
+    private func configureUI() {
+        configureSegmentedControl()
+        photoButton.setTitle("📷", for: .normal)
+    }
+
+    private func configureSegmentedControl() {
+        typeSegmentedControl.removeAllSegments()
+        for type in Pokemon.PokemonType.allTypes {
+            typeSegmentedControl.insertSegment(withTitle: type.emojiValue, at: type.rawValue, animated: false)
+        }
+        typeSegmentedControl.selectedSegmentIndex = 0
+    }
+
+    private func selectedType() -> Pokemon.PokemonType {
+        let index = typeSegmentedControl.selectedSegmentIndex
+        guard let type = Pokemon.PokemonType(rawValue: index) else { fatalError() }
+        return type
+    }
+
 }
 
